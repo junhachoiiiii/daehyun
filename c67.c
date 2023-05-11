0a90041b9116688dd0d0b422e49fca2c32e50fe8
junhachoiiiii <133177256+junhachoiiiii@users.noreply.github.com>
2023-05-11 10:01:42 +0900

    c언어
    2학년 5/11까지 자료

---
+++ c67.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a=2147483648;
+    printf("a:%d\n",a);
+    return 0;
+}
---
+++ c82.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char *name="World";
+    puts("hello");
+    puts(name);
+    printf("%s/n", name);
+    return 0;
+}
